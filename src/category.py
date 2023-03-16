7f665ad59eeefbd58b4307c3be552a654ddadfb9
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-16 18:42:43 +0000

    Added to init and deposit

--- src/category.py
+++ src/category.py
@@ -1,9 +1,30 @@
 class Category:
     def __init__(self):
-        pass
-    def withdraw(self):
-        pass
-    def deposit(self):
-        pass
+        self.name = name
+        self.ledger = []
+    def withdraw(self, wit_amount, wit_description):
+        
+        print(f'{self.name} you withdrew ${wit_amount}.')
+
+    def deposit(self, dep_amount, dep_description):
+        self.dep_amount = 0
+        self.dep_description = ""
     def get_balance(self):
         pass
+
+
+
+    # deposit: 
+        # accepts an amount and description
+        # IF no description is given, it should default to an empty
+
+    # withdraw:
+        # accepts an amount and description
+        # amount should be stored in ledger as a negative number
+        # IF theres not enough funds, nothing happens to the ledger
+        # this method should True if withdrawl took place, and False otherwise
+
+    # get_balance
+        # returns current_balance of budget based on DEPOSIT's and WITHDRAW's that occured
+
+    # transfer
