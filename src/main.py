847f79b8d4a657d7954a3bba2342f9b0e14286c3
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-03-17 18:21:59 +0000

    Budget App so far: Amount input

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from category import Category
+# from category import Category
 
-initial_deposit.deposit (int(input('How much would you like to deposit? ')), 'Deposit')
-initial_deposit.withdrawal(int(input('How much would you like to withdraw? ')), 'Withdraw')
-initial_deposit.get_balance()
-initial_deposit.transfer(1200, rent)
-initial_deposit.transfer(500, utilities)
-initial_deposit.transfer(550, car_payment)
-initial_deposit.transfer(1000, savings)
-initial_deposit.get_balance()
+# initial_deposit.deposit (int(input('How much would you like to deposit? ')), 'Deposit')
+# initial_deposit.withdrawal(int(input('How much would you like to withdraw? ')), 'Withdraw')
+# initial_deposit.get_balance()
+# initial_deposit.transfer(1200, rent)
+# initial_deposit.transfer(500, utilities)
+# initial_deposit.transfer(550, car_payment)
+# initial_deposit.transfer(1000, savings)
+# initial_deposit.get_balance()
 
